7dbdcbb7da088891f51d1fd9a0f9bbde2f5fd663
zyqiu <zyqiu@i9i8.com>
2018-03-26 09:38:22 +0800

    设置时区

--- src/php/lib/start.php
+++ src/php/lib/start.php
@@ -4,4 +4,6 @@ $APP_ROOT = realpath(__DIR__ . '/../../..') . '/';
 
 require_once $APP_ROOT . '/vendor/autoload.php';
 
+date_default_timezone_set("Asia/Shanghai");
+
 $container = new \Pimple\Container();
