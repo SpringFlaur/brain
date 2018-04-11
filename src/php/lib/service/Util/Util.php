2505e9c0e8014360d1fce0cc978b47e2f9dd9b2b
zyqiu <zyqiu@i9i8.com>
2018-04-11 14:50:12 +0800

    增加工具方法

--- src/php/lib/service/Util/Util.php
+++ src/php/lib/service/Util/Util.php
@@ -4,12 +4,32 @@ namespace brain\service\Util;
 
 //工具类
 class Util {
-    //取config.php中的配置
+    /**
+     * 取config.php中的配置
+     * @param $key
+     * @param null $default
+     * @return null
+     */
     public static function conf($key, $default = null) {
         global $container;
         $conf = $container['siteConf'];
         if (isset($conf[$key])) {
             return $conf[$key];
+        } else {
+            return $default;
+        }
+    }
+
+    /**
+     * 取请求中的自定义header参数
+     * @param $key
+     * @param null $default
+     * @return null
+     */
+    public static function headerConf($key, $default = null) {
+        $key = 'HTTP_' . strtoupper($key);
+        if (isset($_SERVER[$key])) {
+            return $_SERVER[$key];
         } else {
             return $default;
         }
